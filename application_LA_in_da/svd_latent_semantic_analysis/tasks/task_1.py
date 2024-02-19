527c8b8d571872f89788c8a9df9c3b73125b4cd1
zinov <savolas@yandex.ru>
2024-02-19 12:10:04 +0300

    - chore!: gitignore

---
+++ application_LA_in_da/svd_latent_semantic_analysis/tasks/task_1.py
@@ -0,0 +1,26 @@
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+
+preprocessed_texts = np.array(['игра крутой большой онлайн',
+                               'огурец отстой не вкусный',
+                               'огурчик супер',
+                               'обычный огурчик понравиться соленый',
+                               'игра ужасный геймплей донат',
+                               'лопата шикарный взять на дача',
+                               'вкусный как на дача',
+                               'дрель мощь просверливать весь стена',
+                               'огурец понравиться свежий',
+                               'вкусный немного горьковатый'])
+
+# Строим матрицу встречаемости слов.
+count_model = CountVectorizer()
+X = count_model.fit_transform(preprocessed_texts).toarray().astype(float).T
+words = count_model.get_feature_names()
+
+
+# Функция возвращает индекс ближайшего к `search_vector` вектора из строк `vectors`.
+def find_nearest_vector_id(search_vector, vectors):
+    # L2 расстояние между векторами.
+    distances = np.mean((search_vector - vectors) ** 2, axis=1)
+    # Индексы векторов с наименьшим расстоянием.
+    return np.argmin(distances)
